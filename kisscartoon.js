4053b9a0f9016eee0b842ac5390704e231229b2e
sv26031 <sonal_vedi@hotmail.com>
2015-08-31 11:04:41 +0100

    Update kisscartoon.js

--- kisscartoon.js
+++ kisscartoon.js
@@ -61,7 +61,6 @@ if (videoQuality === null || videoQuality == '') {
 var i;
 var long_url;
 var newLinks = '';
-var newLinks2 = '';
 var c = startEpisode;
 for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEpisode); i--) {
 	jQuery.ajax({
@@ -92,7 +91,6 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 			}
 			console.log(c);
 			newLinks = newLinks + '<a href="' + long_url + '">Episode ' + c + ' (' + videoQuality + ')</a><br></br>\n';
-			newLinks2 = newLinks2 + '<a href="' + long_url + '">' + long_url + '</a><br></br>\n';
 			c++
         },
         async:   false, 
@@ -104,7 +102,6 @@ var newPageText = 'Use an addon like DownThemAll! to download the episodes on th
 newPageText += 'To download them individually, right click the link and choose Save As. <br></br>'
 newPageText += 'NOTE: If watching episodes from this list, open them in a new tab as you will not be able to come back.<br></br>'
 newPageText += newLinks
-newPageText += newLinks2
 
 var newPage = window.open();
 newPage.document.body.innerHTML = newPageText
