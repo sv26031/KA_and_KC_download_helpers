eca38619074f56adbb60ba8d4e5e4fb944596383
sv26031 <aloochat.b@gmail.com>
2016-03-24 10:47:41 +0000

    Update kissanime.js

--- kissanime.js
+++ kissanime.js
@@ -74,6 +74,7 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 			var stringStart = result.search("var wra"); 
 			var stringEnd = result.search("document.write"); 
 			var javascriptToExecute = result.substring(stringStart, stringEnd);
+			console.log("jsToExec length = " + javascriptToExecute.length)
 			eval(javascriptToExecute);
 			
 			$("body").append('<div id="episode' + i + '" style="display: none;"></div>')
